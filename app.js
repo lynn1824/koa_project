87a5fe76115fac586251ae2728a995be3c6fead2
lynn1824 <lynn1824@yeah.net>
2019-09-02 22:42:24 +0800

    feat:project update

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
-function app() {
-    console.log('hello!!!');
-}
+const Koa = require('koa');
+const axios = require('axios');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+// classic/index
+router.get('/classic/index', (ctx, next) => {
+    ctx.body = {
+        name: 'may13149',
+        age: 30
+    };
+});
+
+// 将router挂载到中间件
+app.use(router.routes()).use(router.allowedMethods());
+
+// app.use(async (ctx, next) => {
+//     await next();
+//     console.log('结果：', ctx.r);
+//     if (ctx.method === 'GET' && ctx.path === '/classic/index') {
+//         ctx.body = {
+//             name: 'May13149',
+//             age: 30
+//         };
+//     }
+// });
+
+// app.use(async (ctx, next) => {
+//     // const res = await axios.get('https://jiazhang.qq.com/jz/home.html'); // 开始阻塞
+//     console.log('path', ctx.path);
+//     console.log('method', ctx.method);
+//     ctx.r = `测试`;
+// });
+
+app.listen(3000);
+console.log('service is staring..., port is 3000');
